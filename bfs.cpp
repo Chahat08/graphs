2e24044ca2587cfa8a92d76848930c5a261b1628
Chahat08 <chahat.ck88@gmail.com>
2022-05-02 20:39:21 +0530

    program to for bfs added

---
+++ bfs.cpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <queue>
+#include "graph.h"
+
+using namespace std;
+
+void bfs(Graph g, char start_node)
+{
+	queue<char> q;
+	q.push(start_node);
+
+	while (!q.empty())
+	{
+		char c = q.front();
+		vector<char> neighbours = g.get_neighbours(c);
+		for (char ch : neighbours) q.push(ch);
+		cout << c << ' ';
+		q.pop();
+	}
+	cout << endl;
+}
+
+int main()
+{
+	Graph g;
+
+	g.add_node('a');
+	g.add_node('b');
+	g.add_node('c');
+	g.add_node('d');
+	g.add_node('e');
+	g.add_node('f');
+	
+	g.add_edge('a', 'c');
+	g.add_edge('a', 'b');
+	g.add_edge('b', 'd');
+	g.add_edge('d', 'f');
+	g.add_edge('c', 'e');
+
+	bfs(g, 'a');
+
+	return 0;
+}
